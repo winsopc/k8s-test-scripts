f3b72eb2f9eb5a42db452239ccffad32e77db694
Winson Wang <windson.wang@gmail.com>
2020-07-16 09:54:15 -0700

    add k8s workload setup
    Signed-off-by: Winson Wang <windson.wang@gmail.com>

---
+++ k8s-setup/setupguestbook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-master-deployment.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-master-service.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-deployment.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-service.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-deployment.yaml
+kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-service.yaml
